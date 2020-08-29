fa03aa55a1aad73b32ae1cbb6ccb9da28021e55d
likun <likun205743@sogou-inc.com>
2020-08-29 16:31:19 +0800

    :rotating_light: injection api

--- src/components/Injection.vue
+++ src/components/Injection.vue
@@ -1,19 +1,30 @@
 <template>
-  <div class="module injection" :style="boxSyle">Injection! {{count}}</div>
+  <div class="module injection" :style="boxSyle">
+    <h3>Injection.</h3>
+    <p>
+      <provideAndInject />
+    </p>
+  </div>diuv
 </template>
 
 <script lang="js">
 import {
     defineComponent,
-    ref
+    provide  // 父组件引入 provide
 } from "vue"
 
 import getRandomColor from "../libs/getRandomColor";
+import provideAndInject from "./provideAndInject.vue";  // 引入子组件
+
 
 //随机颜色值 16hex
 const colorValue = getRandomColor();
 
 export default defineComponent({
+    name: "injectAncestor",
+    components: {
+        provideAndInject
+    },
     data(){
         return{
             boxSyle: {
@@ -22,10 +33,15 @@ export default defineComponent({
         }
     },
     setup() {
-        
-        const count = ref(0);
+
+        const obj = {
+            "data": "我是父组件向子组件传递的值"
+        }
+        // provide('数据名称', 要传递的数据)
+        provide("customVal", obj.data); 
+
         return {
-            count
+            // 
         }
     }
 })
---
+++ src/components/provideAndInject.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <h3>{{ customVal }}</h3>
+  </div>
+</template>
+ 
+<script>
+// 子组件导入 inject
+import { inject } from "vue";
+
+export default {
+  setup() {
+    //调用 inject 函数，通过指定的数据名称，获取到父级共享的数据
+    const customVal = inject("customVal");
+    return {
+      customVal,
+    };
+  },
+};
+</script>
