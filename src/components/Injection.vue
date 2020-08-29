ba633828812e3859df8a09761b63135874d03f33
likun <likun205743@sogou-inc.com>
2020-08-29 16:35:51 +0800

    :whale: injection api.

--- src/components/Injection.vue
+++ src/components/Injection.vue
@@ -1,10 +1,21 @@
+<!--
+    * 说明:
+    * provide() 和 inject() 可以实现嵌套组件之间的数据传递
+    * 这两个函数只能在 setup() 函数中使用
+    * 父级组件中使用 provide() 函数向下传递数据
+    * 子级组件中使用 inject() 获取上层传递过来的数据
+    * 不限层级
+    * 父组件可以通过ref创建响应式数据通过provide 共享给子组件
+-->
+
+
 <template>
   <div class="module injection" :style="boxSyle">
     <h3>Injection.</h3>
     <p>
       <provideAndInject />
     </p>
-  </div>diuv
+  </div>
 </template>
 
 <script lang="js">
@@ -15,7 +26,6 @@ import {
 
 import getRandomColor from "../libs/getRandomColor";
 import provideAndInject from "./provideAndInject.vue";  // 引入子组件
-
 
 //随机颜色值 16hex
 const colorValue = getRandomColor();
