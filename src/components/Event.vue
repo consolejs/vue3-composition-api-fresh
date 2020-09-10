80d5470e40b697c6e000fd12298f9b062c128c21
consolejs <https200@126.com>
2020-09-10 18:18:12 +0800

    :sparkles: Methods in Inline Handlers

--- src/components/Event.vue
+++ src/components/Event.vue
@@ -2,6 +2,7 @@
 <div id="example-2">
     <!-- `greet` 是在下面定义的方法名 -->
     <button @click="greet">Greet</button>
+    <button @click="say('hi', $event)">say hi</button>
 </div>
 </template>
 
@@ -18,13 +19,20 @@ export default defineComponent({
     },
     // 在 `methods` 对象中定义方法
     methods: {
-        greet: function (event) {
+        greet(event) {
             // `this` 在方法里指向当前 Vue 实例
             console.log('Hello ' + this.name + '!')
             // `event` 是原生 DOM 事件
             if (event) {
                 console.log(event.target.tagName)
             }
+        },
+        say(msg, event) {
+            // 现在我们可以访问原生事件对象
+            if (event) {
+                console.log(event.target.tagName)
+            }
+            alert(msg)
         }
     }
 })
